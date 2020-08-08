562484e04b6c40fe506d396fa6f661930044d7bb
Gabs <gaby.cov16@gmail.com>
2020-08-07 20:46:20 -0500

    Last changes

---
+++ build/precache-manifest.0d564f2bfdd53b8a8fbd0cfe9ee4604d.js
@@ -0,0 +1,238 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0343c7899cb32f26de2fb0db2b15de9f",
+    "url": "/ux-portfolio/index.html"
+  },
+  {
+    "revision": "b7541ff06dae05ee05bc",
+    "url": "/ux-portfolio/static/css/main.2902fb66.chunk.css"
+  },
+  {
+    "revision": "aa4c1bb56ef86ff846a1",
+    "url": "/ux-portfolio/static/js/2.ad9fcc61.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ux-portfolio/static/js/2.ad9fcc61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7541ff06dae05ee05bc",
+    "url": "/ux-portfolio/static/js/main.adffac01.chunk.js"
+  },
+  {
+    "revision": "80fa7efb1b12d2fc515f",
+    "url": "/ux-portfolio/static/js/runtime-main.326c0fa5.js"
+  },
+  {
+    "revision": "055fd79dce158de6e3ac24d00c93358d",
+    "url": "/ux-portfolio/static/media/1200px-Logo_Universidad_Anahuac.svg.055fd79d.png"
+  },
+  {
+    "revision": "47a85e6d60fc75a607cfff69e4f2fa1f",
+    "url": "/ux-portfolio/static/media/2x2.47a85e6d.jpg"
+  },
+  {
+    "revision": "8a819c83040a6d74d70a550d6d714924",
+    "url": "/ux-portfolio/static/media/97-975298_image-result-for-accenture-logo-accenture-logo-lgbt.8a819c83.png"
+  },
+  {
+    "revision": "59d6a59b4e1122cea42ebb916ed509dd",
+    "url": "/ux-portfolio/static/media/LOGO-VERTICAL-2009p.59d6a59b.jpg"
+  },
+  {
+    "revision": "fbf77bfde6a78147c69e18d748f43f05",
+    "url": "/ux-portfolio/static/media/after-cd-vinyl.fbf77bfd.png"
+  },
+  {
+    "revision": "8d69068106f97b947f61fb171f5850cb",
+    "url": "/ux-portfolio/static/media/after-checkout.8d690681.png"
+  },
+  {
+    "revision": "f5e4520b6b8c294853b96cab86b76e96",
+    "url": "/ux-portfolio/static/media/after-genres.f5e4520b.png"
+  },
+  {
+    "revision": "c54941c4395cc7dc075c8b106e258271",
+    "url": "/ux-portfolio/static/media/after-register.c54941c4.png"
+  },
+  {
+    "revision": "f1748aa07a24b6a608d2d1a56cdbbd35",
+    "url": "/ux-portfolio/static/media/after-signin.f1748aa0.png"
+  },
+  {
+    "revision": "5f1f045f358be8f8d19e7728bb4e177f",
+    "url": "/ux-portfolio/static/media/banner.5f1f045f.png"
+  },
+  {
+    "revision": "40bdc90ce4e6793d15d91e0d922764ec",
+    "url": "/ux-portfolio/static/media/banner3.40bdc90c.png"
+  },
+  {
+    "revision": "bbbe663ab486e32c39e765cb42b771e5",
+    "url": "/ux-portfolio/static/media/before-cd-vinyl.bbbe663a.png"
+  },
+  {
+    "revision": "5c0ddfc6cf7dc9da1e4058663c046824",
+    "url": "/ux-portfolio/static/media/before-checkout.5c0ddfc6.png"
+  },
+  {
+    "revision": "fef13e38bdddd7d9a64a6cf342cca27f",
+    "url": "/ux-portfolio/static/media/before-genres.fef13e38.png"
+  },
+  {
+    "revision": "3ff0283b1ef6d66406ff462b25db2187",
+    "url": "/ux-portfolio/static/media/before-homepage.3ff0283b.png"
+  },
+  {
+    "revision": "4c38dfde4936dbf447b69264f439e55e",
+    "url": "/ux-portfolio/static/media/before-register.4c38dfde.png"
+  },
+  {
+    "revision": "d2a92bb627a89c84f9949daf87aab54d",
+    "url": "/ux-portfolio/static/media/before-signin.d2a92bb6.png"
+  },
+  {
+    "revision": "4ce4633fe9117c59b0e5c57c71827de0",
+    "url": "/ux-portfolio/static/media/blueish.4ce4633f.png"
+  },
+  {
+    "revision": "6dfe980bde0e434c39d72de47651b2e6",
+    "url": "/ux-portfolio/static/media/card-sorting.6dfe980b.jpg"
+  },
+  {
+    "revision": "d5cda064d535d9a8b6ab69826fb29979",
+    "url": "/ux-portfolio/static/media/conversation2.d5cda064.svg"
+  },
+  {
+    "revision": "bde06829784bbc7c3b4a8e3fdc417bd4",
+    "url": "/ux-portfolio/static/media/customer-journey.bde06829.png"
+  },
+  {
+    "revision": "319cf8af1804631dedb60d7bfc01b0ab",
+    "url": "/ux-portfolio/static/media/design-process-ecolife.319cf8af.png"
+  },
+  {
+    "revision": "f8de3588163b73eff86b00963b57bb3a",
+    "url": "/ux-portfolio/static/media/design-process-mixup.f8de3588.png"
+  },
+  {
+    "revision": "6cad5fdcd2e3d1c2a99e6b4b60151682",
+    "url": "/ux-portfolio/static/media/ecolife.6cad5fdc.svg"
+  },
+  {
+    "revision": "152a6465173c8d328b9b28df260da0c9",
+    "url": "/ux-portfolio/static/media/ecowisdom.152a6465.png"
+  },
+  {
+    "revision": "2705e68a4b24acb6e6c29e15f0f8688d",
+    "url": "/ux-portfolio/static/media/green-star-void.2705e68a.svg"
+  },
+  {
+    "revision": "000d5fc8c3bcc09d40c880f5594fea5c",
+    "url": "/ux-portfolio/static/media/green-star.000d5fc8.svg"
+  },
+  {
+    "revision": "cc2a5b5064d5cb440870c5fd10de78bf",
+    "url": "/ux-portfolio/static/media/greenie.cc2a5b50.png"
+  },
+  {
+    "revision": "0308c64a6ba826483841b115c314bd48",
+    "url": "/ux-portfolio/static/media/homepage.0308c64a.png"
+  },
+  {
+    "revision": "2e5efbd46964628cfafbaad227d28ee6",
+    "url": "/ux-portfolio/static/media/idf.2e5efbd4.jpg"
+  },
+  {
+    "revision": "537e622b7d456b86ed24ca0003ef5704",
+    "url": "/ux-portfolio/static/media/imagotiponaranjacool.537e622b.svg"
+  },
+  {
+    "revision": "b887e4d82042a8819d7fc1ac9e015efb",
+    "url": "/ux-portfolio/static/media/line.b887e4d8.svg"
+  },
+  {
+    "revision": "1dee2bf5887f533bd8cdfaf34cdd26b8",
+    "url": "/ux-portfolio/static/media/linkedin.1dee2bf5.svg"
+  },
+  {
+    "revision": "223a0a7b87c1a7006604da0513752bea",
+    "url": "/ux-portfolio/static/media/linkedin2.223a0a7b.svg"
+  },
+  {
+    "revision": "5ec134e736787702f455d95581512ef3",
+    "url": "/ux-portfolio/static/media/logo.5ec134e7.svg"
+  },
+  {
+    "revision": "7aa0f56aa8c163622b6efab6a7b81627",
+    "url": "/ux-portfolio/static/media/logros.7aa0f56a.png"
+  },
+  {
+    "revision": "4ef0c1addd434054ff6ed3963552cd4e",
+    "url": "/ux-portfolio/static/media/low-fi.4ef0c1ad.jpg"
+  },
+  {
+    "revision": "bab99cd225d28978ca6d1647a7ab5fdf",
+    "url": "/ux-portfolio/static/media/mail.bab99cd2.svg"
+  },
+  {
+    "revision": "48153e0215b52d26c32ca415cd6748ee",
+    "url": "/ux-portfolio/static/media/mail2.48153e02.svg"
+  },
+  {
+    "revision": "e622be45c5dd075c1d4216522a522fc3",
+    "url": "/ux-portfolio/static/media/mejoras.e622be45.png"
+  },
+  {
+    "revision": "c345caf3603e3f71f8447667daa9adae",
+    "url": "/ux-portfolio/static/media/misiones.c345caf3.png"
+  },
+  {
+    "revision": "cc169d26d3f5c21be90dc4eee9e26ec3",
+    "url": "/ux-portfolio/static/media/mixup.cc169d26.svg"
+  },
+  {
+    "revision": "04656bd45883056ca9772da3d81a25bb",
+    "url": "/ux-portfolio/static/media/moi.04656bd4.jpg"
+  },
+  {
+    "revision": "d6bafb632f1f11b4cb05142ce6b498fc",
+    "url": "/ux-portfolio/static/media/persona-mixup.d6bafb63.png"
+  },
+  {
+    "revision": "341f72a4aa8f674ba72f38026e268f6c",
+    "url": "/ux-portfolio/static/media/persona1-ecolife.341f72a4.png"
+  },
+  {
+    "revision": "9529aa99c98815025083b0803a8f4688",
+    "url": "/ux-portfolio/static/media/persona2-ecolife.9529aa99.png"
+  },
+  {
+    "revision": "6903f51f587727ad3291ed570464798f",
+    "url": "/ux-portfolio/static/media/problem-spots.6903f51f.png"
+  },
+  {
+    "revision": "6238a3aa3471d330aaf9c0f0611b2374",
+    "url": "/ux-portfolio/static/media/settings.6238a3aa.png"
+  },
+  {
+    "revision": "453da236b3d7c62f07187a3c78d1cb34",
+    "url": "/ux-portfolio/static/media/story-board.453da236.jpg"
+  },
+  {
+    "revision": "546712bcc1d1585957699088fbf8c800",
+    "url": "/ux-portfolio/static/media/success-rate-after.546712bc.png"
+  },
+  {
+    "revision": "102b7d94fafb31ba2d74564993166e5a",
+    "url": "/ux-portfolio/static/media/success-rate-mixup.102b7d94.png"
+  },
+  {
+    "revision": "fcb472218dff1655ac6ff9c7ff31e45e",
+    "url": "/ux-portfolio/static/media/tutorial.fcb47221.png"
+  },
+  {
+    "revision": "d5c49a0a80efa190c73f47e4eb16e0da",
+    "url": "/ux-portfolio/static/media/tutoriales.d5c49a0a.png"
+  }
+]);
