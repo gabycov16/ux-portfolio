2e9daa2ccef772bdbd343a3eff4a5451391f83d0
Gabs <gaby.cov16@gmail.com>
2020-11-22 17:02:27 -0600

    Header clean and responsive portfolio with Bootstrap

--- src/pages/portfolio/portfolio.component.jsx
+++ src/pages/portfolio/portfolio.component.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./portfolio.styles.scss";
+import { Container, Row, Col } from "react-bootstrap";
 import Header from "../../components/header/header.component";
 import Mixup from "../../assets/mixup.svg";
 import Ecolife from "../../assets/ecolife.svg";
@@ -8,47 +9,46 @@ const Portfolio = ({ history, linkUrl, match }) => (
   <div className="king-container">
     <div className="homepage">
       <Header />
-      <div id={"features"}>
-        <div
-          className="case-studies"
-          onClick={() => history.push("/ux-portfolio/ecolife")}
-        >
-          <div
-            className="case-studies__image"
-            style={{ backgroundImage: `url(${Ecolife})` }}
-          >
-            <div className="info-container">
-              <div class="case-studies__info case-studies__location">
-                <p className="subtitle-info">A new way to help nature</p>
-                <div class="case-studies__b">
-                  <p className="button-text">VIEW CASE STUDY</p>
-                </div>
+      <Container>
+        <Row  onClick={() => history.push("/ux-portfolio/ecolife")}>
+          <Col xs={12} sm={6} md={6} xl={6}  lg={6} className="left-info">
+            <Col >
+              <p className="subtitle-info">A new way to help nature</p>
+            </Col>
+            <Col>
+              <div class="case-studies__b" >
+                <p className="button-text">VIEW CASE STUDY</p>
               </div>
-            </div>
-          </div>
-        </div>
-        <div
-          className="case-studies"
-          onClick={() => history.push("/ux-portfolio/mixup")}
-        >
-          <div
-            className="case-studies__image"
-            id="cs"
-            style={{ backgroundImage: `url(${Mixup})` }}
-          >
-            <div className="info-container">
-              <div class="case-studies__info case-studies__location">
-                <p className="subtitle-info">Music & technology choice</p>
-                <div class="case-studies__b">
-                  <p className="button-text">VIEW CASE STUDY</p>
-                </div>
+            </Col>
+          </Col>
+          <Col className="right-info">
+            <div
+              className="case-studies__image"
+              style={{ backgroundImage: `url(${Ecolife})` }}
+            ></div>
+          </Col>
+        </Row>
+      
+        <Row onClick={() => history.push("/ux-portfolio/mixup")}>
+          <Col xs={12} sm={6} md={6} xl={6}  lg={6} className="left-info">
+            <Col >
+              <p className="subtitle-info">Music & technology choice</p>
+            </Col>
+            <Col>
+              <div class="case-studies__b" >
+                <p className="button-text">VIEW CASE STUDY</p>
               </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    
-       <div className="contact">
+            </Col>
+          </Col>
+          <Col className="right-info">
+            <div
+              className="case-studies__image"
+              style={{ backgroundImage: `url(${Mixup})` }}
+            ></div>
+          </Col>
+        </Row>
+      </Container>
+      <div className="contact">
         <span className="contact-title">Got a project in mind?</span>
         <span className="contact-subtitle">Feel free to reach me:</span>
         <a className="email" href="mailto:gabriela.co.design@gmail.com">
